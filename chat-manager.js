7d93251653ce01126b8d6feccee2431523519a65
teoWei <54856793+teoWei@users.noreply.github.com>
2024-11-23 20:23:31 +0800

    Polish and test response

--- chat-manager.js
+++ chat-manager.js
@@ -1,3 +1,6 @@
+let msgCD = false;
+let llmProccessing = false;
+
 function resizeTextarea(elem) {
     if (!elem.style.height) return elem.style.height = "1.2em";
     const textbox = document.getElementById("user-text-box")
@@ -5,12 +8,24 @@ function resizeTextarea(elem) {
     if (!chatWindow.style.height) return chatWindow.style.height = "89.75vh";
     if (elem.clientHeight < elem.scrollHeight) {
         if (textbox.style.flexDirection == "column") {
-            if (elem.style.height == "9.6em") return;
-            elem.style.height = `${Number(elem.style.height.substr(0,elem.style.height.indexOf("em")))+1.2}em`;
-            chatWindow.style.height = `calc(${chatWindow.style.height} - 1.75em)`
+            let rescale = true
+            while (rescale) {
+                if (elem.style.height == "9.6em") return;
+                elem.style.height = `${Number(elem.style.height.substr(0,elem.style.height.indexOf("em")))+1.2}em`;
+                chatWindow.style.height = `calc(${chatWindow.style.height} - 1.75em)`
+                if (elem.clientHeight < elem.scrollHeight){} else return;
+            };
         } else {
             textbox.style.flexDirection = "column";
             chatWindow.style.height = `calc(${chatWindow.style.height} - 4.5em)`;
+            if (elem.clientHeight < elem.scrollHeight){} else return;
+            let rescale = true
+            while (rescale) {
+                if (elem.style.height == "9.6em") return;
+                elem.style.height = `${Number(elem.style.height.substr(0,elem.style.height.indexOf("em")))+1.2}em`;
+                chatWindow.style.height = `calc(${chatWindow.style.height} - 1.75em)`
+                if (elem.clientHeight < elem.scrollHeight){} else return;
+            };
         };
     } else {
         if (elem.style.height == "1.2em") {
@@ -39,8 +54,33 @@ function resizeTextarea(elem) {
     };
 };
 
-function sendMessage() {
-    s
+async function sendMessage(identifier, content) {
+    const chatWindow = document.getElementById("chat-box-window");
+    let messageClass;
+    if (identifier == "user") {
+        messageClass = "user-message";
+        chatWindow.innerHTML = `<div class="${messageClass}">\n<p>\n${content}\n</p>\n</div>\n${chatWindow.innerHTML}`;
+    } else if (identifier == "assistantProcessing") {
+        messageClass = "assistant-message";
+        chatWindow.innerHTML = `<div id="assistant-processing" class="${messageClass}">\n<p>\n\n</p>\n</div>\n${chatWindow.innerHTML}`;
+        const messageBox = document.getElementById("assistant-processing");
+        while (llmProccessing) {
+            await new Promise(resolve => setTimeout(resolve, 500));
+            if (!llmProccessing) return;
+            messageBox.innerHTML = `<p>.</p>`;
+            await new Promise(resolve => setTimeout(resolve, 500));
+            if (!llmProccessing) return;
+            messageBox.innerHTML = `<p>..</p>`;
+            await new Promise(resolve => setTimeout(resolve, 500));
+            if (!llmProccessing) return;
+            messageBox.innerHTML = `<p>...</p>`;
+        };
+    } else if (identifier == "assistant") {
+        messageClass = "assistant-message";
+        const messageBox = document.getElementById("assistant-processing");
+        messageBox.innerHTML = `<p>\n${content}\n</p>`;
+        messageBox.removeAttribute('id');
+    };
 };
 
 async function generateText(prompt) {
@@ -54,5 +94,36 @@ async function generateText(prompt) {
   
     const data = await response.json();
     console.log(data.text);   
-  
-}
+    sendMessage("assistant", data.text);
+};
+
+function processPrompt(){
+    const textArea = document.getElementById("user-text-input");
+    if(!textArea.value || msgCD) return;
+    msgCD = true;
+    sendMessage("user", textArea.value);
+    //generateText(textArea.value);
+    testResponse(textArea.value)
+    textArea.value = null;
+    resizeTextarea(textArea);
+};
+
+async function testResponse(prompt) {
+    llmProccessing = true;
+    sendMessage("assistantProcessing")
+    setTimeout(()=>{ sendMessage("assistant", prompt) },5000);
+    setTimeout(()=>{
+        msgCD = false;
+        llmProccessing = false;
+    },5000);
+};
+
+function textareaKeyPressed(e) {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        if (e.shiftKey || msgCD) return;
+        document.getElementById("send-btn").click();
+    };
+};
+
+//Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis mauris nibh, ac pharetra urna ultricies sit amet. Cras bibendum pretium tellus. Nulla enim purus, fermentum eget orci vitae, pretium aliquet metus. Curabitur sagittis purus nisi, et ultrices tellus eleifend quis. Pellentesque nisi eros, ornare nec mattis nec, elementum ac odio. Donec at bibendum tellus, eget mattis justo. Donec id varius felis. Pellentesque sit amet egestas nunc. Vivamus a urna consequat, volutpat lectus ac, tincidunt dui. Duis sed urna luctus, imperdiet mauris sit amet, volutpat turpis. Nullam auctor mi eget turpis vestibulum, nec ultrices augue sodales. Vivamus at lobortis orci. Phasellus vel augue id elit maximus bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus.
